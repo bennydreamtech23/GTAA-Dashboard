36892c9f940e2c049b2a5209d8205ae4e83bb5d5
bennydreamtech23 <101554359+bennydreamtech23@users.noreply.github.com>
2022-10-20 22:15:03 +0100

    Files for html, CSS and JavaScript for the project

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+//body modal
+function bodyModal(){
+  swal({
+    title: "Welcome to GTAA-Passenger Wait Time",
+    text: "we happy you visted our website, Thank you!!!",
+    icon: "success",
+    button: "Aww yiss!",
+  });
+}
+//bodyModal()
+
+//alert(bodyModal())
+
+
