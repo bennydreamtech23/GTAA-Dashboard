dd8ba0dda22f9cfb0ebaef548a7dbea34b5fce51
bennydreamtech23 <101554359+bennydreamtech23@users.noreply.github.com>
2022-10-21 19:02:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 //body modal
 function bodyModal(){
   swal({
-    title: "Welcome to GTAA-Passenger Wait Time",
+    title: "Welcome to TORONTO PEARSON-Passenger Wait Time",
     text: "we happy you visted our website, Thank you!!!",
     icon: "success",
     button: "Aww yiss!",
